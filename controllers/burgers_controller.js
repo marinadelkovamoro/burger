d1b0f14ab2405458ea96365fcac9d75d63e0f03f
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-01 18:31:24 -0700

    added mvc files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+// Create the router for the app, and export the router at the end of your file.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,16 @@
+var orm = require("../config/orm.js");
+
+// Code that will call the ORM functions using burger specific input for the ORM.
+
+
+
+
+
+
+
+
+
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
+
